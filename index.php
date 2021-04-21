3fc103aff893d9b7b218230b051b3c98b9cf47d4
DerianCordobaPerez <derianunanleon@gmail.com>
2021-04-21 15:20:36 -0600

    Agregando ventana modal a los botones de los usuarios

--- index.php
+++ index.php
@@ -25,6 +25,7 @@
     <!-- Acordeon -->
     <div class="container">
         <div class="accordion" id="accordionExample">
+            <!-- Primer Acordeon -->
             <div class="accordion-item">
                 <h2 class="accordion-header" id="headingOne">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -33,46 +34,14 @@
                 </h2>
                 <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                        <h3><strong>Alumnos</strong></h3>
-                        <h5>Rellene el formulario con los datos de estudiante</h5>
-                        <form action="process.php" method="post" enctype="multipart/form-data">
-                            <div class="form-group">
-                                <label for="email"><strong>Email</strong></label>
-                                <input type="email" name="email" class="form-control" placeholder="Email" required />
-                            </div>
-
-                            <div class="form-group">
-                                <label for="email"><strong>Nombre</strong></label>
-                                <input type="text" name="name" class="form-control" placeholder="Nombre" maxlength="200" required />
-                            </div>
-
-                            <div class="form-group">
-                                <label for="email"><strong>Carnet</strong></label>
-                                <input type="text" name="license" class="form-control" placeholder="Carnet" maxlength="10" required />
-                            </div>
-
-                            <div class="form-group">
-                                <label for="email"><strong>Edad</strong></label>
-                                <input type="number" name="age" class="form-control" placeholder="Edad" min="15" max="50" required />
-                            </div>
-
-                            <div class="form-group">
-                                <label for="email"><strong>Curso actual</strong></label>
-                                <input type="number" name="course" class="form-control" placeholder="Curso actual" min="1" max="5" required>
-                            </div>
-
-                            <div class="form-group">
-                                <label for="email"><strong>Imagen</strong></label>
-                                <input type="file" name="photo" class="form-control" required>
-                            </div>
-
-                            <div class="d-grid gap-2">
-                                <input type="submit" value="Enviar" class="btn btn-primary my-2" />
-                            </div>
-                        </form>
+                        <?php
+                            include 'helpers/form.php';
+                            show_form('process.php');
+                        ?>
                     </div>
                 </div>
             </div>
+            <!-- Segundo Acordeon -->
             <div class="accordion-item">
                 <h2 class="accordion-header" id="headingTwo">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
--- listStudent.php
+++ listStudent.php
@@ -1,4 +1,27 @@
 <?php
+    function window_modal(): void {
+        echo "
+            <div class='modal fade' id='editModal' data-bs-backdrop='static' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+                <div class='modal-dialog modal-dialog-scrollable'>
+                    <div class='modal-content'>
+                        <div class='modal-header'>
+                            <h5 class='modal-title' id='exampleModalLabel'>Editar usuario</h5>
+                            <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                        </div>
+                        <div class='modal-body'>";
+                            show_form('update.php');
+
+        echo "
+                        </div>
+                        <div class='modal-footer'>
+                            <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Cerrar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        ";
+    }
+    window_modal();
     function list_students(): void {
         $titles = array('Nombre', 'Email', 'Edad', 'Carnet', 'Curso');
         $students = explode('\n', file_get_contents('StudentSerialize.txt'));
@@ -19,27 +42,28 @@
         for($i = 0; $i < count($students) - 1; $i++) {
             $attributes = explode(',', $students[$i]);
             echo "
-                                    <div class='row'>
-                                        <div class='col-md-4'>
-                                            <div class='image-shadow'>
-                                                <img class='rounded mx-auto d-block image' src='uploads/$attributes[5]' />
-                                            </div>
-                                        </div>
-                                        <div class='col-md-4'>
-                                ";
+                <div class='row'>
+                    <div class='col-md-4'>
+                        <div class='image-shadow'>
+                            <img class='rounded mx-auto d-block image' src='uploads/$attributes[5]' />
+                        </div>
+                    </div>
+                    <div class='col-md-4'>
+            ";
             for ($j = 0; $j < count($attributes) - 2; $j++)
                 echo "<p><strong>$titles[$j]: </strong> $attributes[$j]</p>";
             echo "</div>";
 
             echo "
-                                    <div class='col-md-4'>
-                                        <button type='button' class='btn btn-warning'>Editar</button>
-                                        <button type='button' class='btn btn-danger'>Eliminar</button>
-                                ";
+                <div class='col-md-4'>
+                    <button type='button' class='btn btn-warning' data-bs-toggle='modal' data-bs-target='#editModal'>Editar</button>
+                    <button type='button' class='btn btn-danger' data-bs-toggle='modal'data-bs-target='#deleteModal'>Eliminar</button>
+                </div>
+            ";
             echo "
-                        </div>
                     </div>
                 <hr />
             ";
         }
     }
+?>
